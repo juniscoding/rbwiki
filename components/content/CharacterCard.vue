ca8c2ef30c8d2bfdc962a4d0cdf77b48e94805d7
Valentina Nakayama <valentina@poli.ufrj.br>
2023-08-22 12:22:45 -0300

    character card improvements

--- components/content/CharacterCard.vue
+++ components/content/CharacterCard.vue
@@ -1,22 +1,8 @@
 <!-- eslint-disable vue/html-indent -->
 <template>
-    <!-- image -->
-    <!-- name -->
-    <!-- table style -->
-    <!-- other names -->
-    <!-- titles -->
-    <!-- birthday (if available) -->
-    <!-- age -->
-    <!-- gender -->
-    <!-- height -->
-    <!-- hair -->
-    <!-- eyes -->
-    <!-- culture -->
-    <!-- spouse -->
-    <!-- race -->
-  <div class="rounded-lg border-2 border-red-900  flex-col place-items-center max-w-sm place-self-end">
+    <div class="rounded-lg border-2 border-red-900  flex-col place-items-center max-w-xs place-self-end">
     <div>
-        <img :src="image" alt="" />
+        <img :src="image" :alt="name" />
     </div>
 
     <div class="flex-col justify-items-center text-center py-3 bg-red-900">
@@ -29,6 +15,49 @@
                 <td>Full name</td>
                 <td>{{ fullname }}</td>
             </tr>
+            <tr>
+                <td>Age</td>
+                <td>{{ age }}</td>
+            </tr>
+            <tr>
+                <td>Other names</td>
+                <td>{{ other_names }}</td>
+            </tr>
+            <tr>
+                <td>Age</td>
+                <td>{{ age }}</td>
+            </tr>
+            <tr>
+                <td>Birthday</td>
+                <td>{{ birthday }}</td>
+            </tr>
+            <tr>
+                <td>Spouse</td>
+                <td>{{ spouse }}</td>
+            </tr>
+        </table>
+    </div>
+            <div class="flex-col justify-items-center text-center py-3 bg-red-900">
+                <p class="rb-title-simple">Physical Characteristics</p>
+            </div>
+    <div class="flex-col justify-items-center text-center">
+        <table>
+            <tr>
+                <td>Race</td>
+                <td>{{ race }}</td>
+            </tr>
+            <tr>
+                <td>Height</td>
+                <td>{{ height }}</td>
+            </tr>
+            <tr>
+                <td>Hair</td>
+                <td>{{ hair }}</td>
+            </tr>
+            <tr>
+                <td>Eyes</td>
+                <td>{{ eyes }}</td>
+            </tr>
         </table>
     </div>
 
@@ -40,6 +69,15 @@ export interface Props {
     name: string;
     fullname: string;
     image: string;
+    age: string;
+    other_names: string;
+    birthday: string;
+    hair: string;
+    height: string;
+    hair: string;
+    eyes: string;
+    spouse: string;
+    race: string;
 }
 
 defineProps<Props>();
@@ -52,5 +90,9 @@ td, th {
   padding: 8px;
 }
 
+td:first-of-type {
+    font-weight: 700;
+    min-wdith: 10rem;
+}
 
 </style>
